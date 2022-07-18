bb0ff08c219e564a554692d3407140ea56125e4a
marcoafloresp <106285939+marcoafloresp@users.noreply.github.com>
2022-07-18 13:32:38 -0300

    js commit

--- js/main.js
+++ js/main.js
@@ -40,23 +40,23 @@ if(edad>=18){
 }
 
 //ingreso unidades
-let can_remeras = parseInt(prompt("Ingrese la cantidad de remeras que desea:"));
+let can_remeras = parseInt(prompt("Ingrese la cantidad de remeras que desea: \n(El precio por unidad es de $ 500.)" ));
 let precio_remera = 500
 var suma_final = precio_remera * can_remeras;
 
 if(can_remeras>10&&can_remeras<50){
     suma_final = suma_final -((suma_final * 5)/100)
     alert ("el total a pagar es de: $ " + suma_final);
-}else if(can_remeras>50){
+}else if(can_remeras>=50){
     suma_final = suma_final -((suma_final * 10)/100)
     alert ("el total a pagar es de: $ " + suma_final);
-}else{
+}else{x
 precio_remera = precio_remera * can_remeras;
 alert("el precio final es de: $ " + precio_remera)
 }
 
 //ingreso variedad
-var col_remeras = prompt("ingrese el color de remera que necesita:");
+var col_remeras = prompt("ingrese el color de remera que necesita:(negro - blanco - rojo - azul");
     switch(col_remeras.toLowerCase()){
     case "negro":
         alert ("Color de remera 'Negro' elegida");
@@ -73,3 +73,10 @@ var col_remeras = prompt("ingrese el color de remera que necesita:");
     default:
             alert("Color no disponible");
 }
+//salida
+salida = "Gracias " + nombre + " " + apellido +"\n";
+salida += "Su pedido de " + can_remeras + " remeras en color " + col_remeras + " fue agregado. \n";
+salida += "El costo final con el descuento incluido si corresponde, es de: $ " + suma_final + "\n";
+
+alert (salida);
+
